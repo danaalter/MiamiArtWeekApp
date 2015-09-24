081cfd59526584bedc3475607475492af1092e1d
ericamunoz02 <erica.munoz002@stu.aii.edu>
2015-09-23 20:12:15 -0400

    adding files

---
+++ dist/js/explore.js
@@ -0,0 +1,10 @@
+ function position(){
+          if( window.location.hash == '#hotel' ){
+              alert("Diplay Hotels");
+          }else if( window.location.hash == '#restaurant' ){
+              alert("Diplay Restaut");
+          }else if( window.location.hash == '#landmark' ){
+              alert("Diplay Landmarks");
+          }
+      }
+position();
